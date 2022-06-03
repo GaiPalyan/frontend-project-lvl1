acd73e4e514f90fe20205737e75a192783de1044
gai <chadreed020788@yandex.ru>
2022-06-03 03:20:06 +0300

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const rounds = 3;
 
 export default (description, gameData) => {
-  console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games');
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}`);
   console.log(description);
